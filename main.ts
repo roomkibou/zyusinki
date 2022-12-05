30f1ba683829244157ac8938facc8c6b259c04d0
roomkibou <113073945+roomkibou@users.noreply.github.com>
2022-12-05 15:43:41 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,23 @@
+radio.onReceivedString(function (receivedString) {
+    信号 = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    ポイント = Math.map(信号, -128, -42, 0, 9)
+    serial.writeValue("x", 信号)
+    serial.writeValue("p", ポイント)
+    led.plotBarGraph(
+    ポイント,
+    9
+    )
+})
+let ポイント = 0
+let 信号 = 0
+radio.setGroup(1)
+basic.showIcon(IconNames.SmallHeart)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
